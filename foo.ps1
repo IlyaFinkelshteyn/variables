ead902d02459f1ec9cfc066cc323f5913d4e00a8
IlyaFinkelshteyn <IlyaFinkelshteyn@users.noreply.github.com>
2016-12-27 16:16:03 -0800

    Update foo.ps1

--- foo.ps1
+++ foo.ps1
@@ -1,3 +1,3 @@
-write-host "myVar before update to foo $env:MyVar"
+write-host "myVar before update to foo: $env:MyVar"
 $env:MyVar = "foo"
-write-host "myVar after update to foo $env:MyVar"
+write-host "myVar after update to foo: $env:MyVar"
